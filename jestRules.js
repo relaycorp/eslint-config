44ea5fabe639bd34340a88db2626d9c9a5c78c05
Gus Narea <gnarea@users.noreply.github.com>
2022-11-24 16:29:47 +0000

    fix(jest): Disable unwanted rules

--- jestRules.js
+++ jestRules.js
@@ -4,5 +4,16 @@ module.exports = {
         {fn: 'test', withinDescribe: 'test'}
     ],
     "jest/prefer-expect-assertions": "off",
-    "jest/prefer-lowercase-title": "off"
+    "jest/prefer-lowercase-title": "off",
+
+    // We'd leave this enabled ideally, but we just have too much code using hooks today
+    "jest/no-hooks": "off",
+
+    "jest-formatting/padding-around-all": "off",
+    'jest-formatting/padding-around-after-all-blocks': 2,
+    'jest-formatting/padding-around-after-each-blocks': 2,
+    'jest-formatting/padding-around-before-all-blocks': 2,
+    'jest-formatting/padding-around-before-each-blocks': 2,
+    'jest-formatting/padding-around-describe-blocks': 2,
+    'jest-formatting/padding-around-test-blocks': 2,
 }
