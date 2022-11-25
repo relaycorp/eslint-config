9b4f1dec2ad12e230cda59b6e1066a1c42be297e
Gus Narea <gnarea@users.noreply.github.com>
2022-11-25 20:08:43 +0000

    fix: Implement workaround to support ESM
    See https://github.com/import-js/eslint-plugin-import/issues/2170#issuecomment-1149473239

--- nodejsRules.js
+++ nodejsRules.js
@@ -9,4 +9,10 @@ module.exports = {
     "unicorn/no-null": "off", // Sound idea but `null` is already used extensively in our codebase
     "unicorn/custom-error-definition": "off", // Good rule but conflicts with another that requires constant properties to be defined at the class level (instead of the constructor)
     "unicorn/consistent-function-scoping": "off",
+
+    // Ugly workaround to support ESM. See:
+    // https://github.com/import-js/eslint-plugin-import/issues/2170#issuecomment-1149473239
+    "import/extensions": ["error", "always", {"ts": "never", "tsx": "never"}],
+    "node/file-extension-in-import": ["error", "always"],
+    "node/no-missing-import": "off",
 };
