f35d7208d16ddcabecf302915a5540dd4ffc5605
Gus Narea <gnarea@users.noreply.github.com>
2022-12-12 10:29:06 +0000

    fix(jest-formatting): Only require padding around `describe` and `test`

--- jestRules.js
+++ jestRules.js
@@ -12,11 +12,6 @@ module.exports = {
     // We'd leave this enabled ideally, but we just have too much code using hooks today
     "jest/no-hooks": "off",
 
-    "jest-formatting/padding-around-all": "off",
-    'jest-formatting/padding-around-after-all-blocks': 2,
-    'jest-formatting/padding-around-after-each-blocks': 2,
-    'jest-formatting/padding-around-before-all-blocks': 2,
-    'jest-formatting/padding-around-before-each-blocks': 2,
-    'jest-formatting/padding-around-describe-blocks': 2,
-    'jest-formatting/padding-around-test-blocks': 2,
+    'jest-formatting/padding-around-describe-blocks': "error",
+    'jest-formatting/padding-around-test-blocks': "error",
 }
