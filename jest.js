83c38a2434b303219a8559d749412a2a8f00f81b
Gus Narea <gnarea@users.noreply.github.com>
2024-09-13 16:20:14 +0100

    fix tests

--- jest.js
+++ jest.js
@@ -16,31 +16,6 @@ export default [
     ...jestPlugin.configs['flat/all'],
     files: TEST_FILE_GLOBS,
   },
-  {
-    rules: {
-      'jest/valid-expect': ['error', { alwaysAwait: true }],
-      'jest/max-expects': ['error', { max: 10 }],
-      'jest/no-restricted-matchers': [
-        'error',
-        {
-          toMatchSnapshot: 'Use more targeted assertions instead of a snapshot',
-          toMatchInlineSnapshot:
-            'Use more targeted assertions instead of a snapshot',
-          toThrowErrorMatchingSnapshot:
-            'Use more targeted assertions instead of a snapshot',
-          toThrowErrorMatchingInlineSnapshot:
-            'Use more targeted assertions instead of a snapshot',
-
-          toBeTruthy:
-            'Use more explicit matchers like `.toBe(true)` or `.toBeGreaterThan(0)`. See https://docs.gitlab.com/ee/development/testing_guide/frontend_testing.html#avoid-using-tobetruthy-or-tobefalsy',
-          toBeFalsy:
-            'Use more explicit matchers like `.toBe(false)`, `.toBe(0)`, or `.toBeUndefined()`. See https://docs.gitlab.com/ee/development/testing_guide/frontend_testing.html#avoid-using-tobetruthy-or-tobefalsy',
-          toBeDefined:
-            'Use a more explicit matcher. See https://docs.gitlab.com/ee/development/testing_guide/frontend_testing.html#tricky-tobedefined-matcher',
-        },
-      ],
-    },
-  },
 
   {
     files: TEST_FILE_GLOBS,
